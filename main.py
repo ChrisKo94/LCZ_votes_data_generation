e1f5e5ee6bdf271b6cbad0d59e40b53370fbd59d
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2021-03-02 16:29:11 +0100

    adapted to validation data

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from torch.distributions import Categorical
 
 from utils.transforms import to_one_hot_majority, to_one_hot, concatenate_cities_labels, concatenate_cities_patches
 
+#split = "random"
+split = "sophisticated"
+
 overall_list = ['amsterdam', 'berlin', 'cologne', 'guangzhou', 'islamabad', 'jakarta',
                 'london', 'losangeles', 'madrid', 'milan', 'moscow', 'mumbai', 'munich',
                 'nairobi', 'paris', 'riodejaneiro', 'rome', 'zurich']
@@ -25,19 +28,28 @@ path_data = "/data/lcz42_votes/data/"
 test_split = 0.2
 n_test_cities = math.ceil(test_split * len(city_list))
 
-np.random.seed(13)
-# Randomly sample 20% of cities as test cities
-test_cities = list(np.array(city_list)[np.random.choice(len(city_list), int(n_test_cities), replace = False)])
-# Take rest as train cities
-train_cities = [city for city in city_list if city not in test_cities]
+if split == "random":
+    np.random.seed(13)
+    # Randomly sample 20% of cities as test cities
+    test_cities = list(np.array(city_list)[np.random.choice(len(city_list), int(n_test_cities), replace=False)])
+    # Take rest as train cities
+    train_cities = [city for city in city_list if city not in test_cities]
 
-n_test_addons = math.ceil(test_split * len(addon_list))
+    n_test_addons = math.ceil(test_split * len(addon_list))
 
-np.random.seed(1313)
-# Randomly sample 20% of addons as test cities
-test_addons = list(np.array(addon_list)[np.random.choice(len(addon_list), int(n_test_addons), replace = False)])
-# Take rest as train addons
-train_addons = [addon for addon in addon_list if addon not in test_addons]
+    np.random.seed(1313)
+    # Randomly sample 20% of addons as test cities
+    test_addons = list(np.array(addon_list)[np.random.choice(len(addon_list), int(n_test_addons), replace=False)])
+    # Take rest as train addons
+    train_addons = [addon for addon in addon_list if addon not in test_addons]
+elif split == "sophisticated":
+    test_cities = ["madrid", "munich", "paris", "zurich"]
+    # Take rest as train cities
+    train_cities = [city for city in city_list if city not in test_cities]
+
+    test_addons = ["losangeles_addon", "moscow_addon", "mumbai_addon", "riodejaneiro_addon"]
+    # Take rest as train addons
+    train_addons = [addon for addon in addon_list if addon not in test_addons]
 
 # Concatenate patches of train cities & addons
 x_train = concatenate_cities_patches(train_cities + train_addons)
@@ -65,6 +77,17 @@ y_test_addons = to_one_hot_majority(y_test_addons, labels, addon=True)
 y_test = np.vstack((y_test_cities, y_test_addons))
 y_test = y_test.astype(int)
 
+if split == "sophisticated":
+    np.random.seed(42)
+    indices_val = np.random.choice(np.arange(y_test.shape[0]), math.ceil(0.5 * y_test.shape[0]), False)
+    indices_test = list(set(np.arange(y_test.shape[0])) - set(indices_val))
+
+    x_val = x_test[indices_val,]
+    x_test = x_test[indices_test]
+
+    y_val = y_test[indices_val,]
+    y_test = y_test[indices_test,]
+
 # Save to file
 
 train_data_h5 = h5py.File(path_data + 'train_data.h5', 'w')
@@ -72,10 +95,17 @@ train_data_h5.create_dataset('x', data=x_train)
 train_data_h5.create_dataset('y', data=y_train)
 train_data_h5.close()
 
+if split == "sophisticated":
+    validation_data_h5 = h5py.File(path_data + 'validation_data.h5', 'w')
+    validation_data_h5.create_dataset('x', data=x_val)
+    validation_data_h5.create_dataset('y', data=y_val)
+    validation_data_h5.close()
+
 test_data_h5 = h5py.File(path_data + 'test_data.h5', 'w')
 test_data_h5.create_dataset('x', data=x_test)
 test_data_h5.create_dataset('y', data=y_test)
 test_data_h5.close()
+
 
 ############################################ Train entropies ###########################################################
 
@@ -89,11 +119,11 @@ y_train_addons = to_one_hot(y_train_addons, labels, addon=True)
 
 y_train_cities = y_train_cities.astype(int)
 y_train_cities = torch.from_numpy(y_train_cities)
-y_train_cities = y_train_cities / 11
+y_train_cities = y_train_cities / y_train_cities.sum(axis=1, keepdims=True)
 
 y_train_addons = y_train_addons.astype(int)
 y_train_addons = torch.from_numpy(y_train_addons)
-y_train_addons = y_train_addons / 9
+y_train_addons = y_train_addons / y_train_addons.sum(axis=1, keepdims=True)
 
 entropies_train_cities = Categorical(probs = y_train_cities).entropy()
 entropies_train_addons = Categorical(probs = y_train_addons).entropy()
@@ -117,17 +147,20 @@ y_test_addons = to_one_hot(y_test_addons, labels, addon=True)
 
 y_test_cities = y_test_cities.astype(int)
 y_test_cities = torch.from_numpy(y_test_cities)
-y_test_cities = y_test_cities / 11
+y_test_cities = y_test_cities / y_test_cities.sum(axis=1, keepdims=True)
 
 y_test_addons = y_test_addons.astype(int)
 y_test_addons = torch.from_numpy(y_test_addons)
-y_test_addons = y_test_addons / 9
+y_test_addons = y_test_addons / y_test_addons.sum(axis=1, keepdims=True)
 
 entropies_test_cities = Categorical(probs = y_test_cities).entropy()
 entropies_test_addons = Categorical(probs = y_test_addons).entropy()
 
 # Concatenate entropies of test cities & addons
 entropies_test = torch.cat((entropies_test_cities, entropies_test_addons), 0)
+
+if split == "sophisticated":
+    entropies_test = entropies_test[indices_test, ]
 
 entropies_test_h5 = h5py.File(path_data + 'entropies_test.h5', 'w')
 entropies_test_h5.create_dataset('entropies_test', data=entropies_test.numpy())
